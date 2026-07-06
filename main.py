9e71fe2ea5560014ae5e65c66afebb72e0bbfa23
Harish <gaddamharish02@gmail.com>
2026-07-06 18:09:07 +0530

    Added IP address validation

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 import sys
-import ipaddress
+from utils.ip_validator import validate_ip
 
 if len(sys.argv) < 2:
     print("[-] Error: Target IP address is required.")
-    print("Usage: py main.py <IP Address>")
+    print("Usage: python main.py <IP Address>")
     sys.exit()
 
 target = sys.argv[1]
 
-try:
-    ipaddress.ip_address(target)
-    print(f"[+] Valid Target: {target}")
-
-except ValueError:
-    print(f"[-] Invalid IP Address: {target}")
+if not validate_ip(target):
+    print("[-] Invalid IP Address")
     sys.exit()
+
+print("[+] Valid Target:", target)
+
     
     
